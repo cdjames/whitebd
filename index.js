82c500bc09f27b85526e5ccf83b743fba658ffd4
Collin James <mail@collinjam.es>
2015-01-24 01:12:37 -0500

    basic layout ready. need to start reformatting for my game. first up is to figure out login with a json file instead of database to start. user and master need to login or be booted back to main page

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+/*app.get forwards urls to specified html page*/
 app.get('/', function(req, res){
   // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
-});
+})
 
-app.get('/master', function(req, res){
+  .get('/user', function(req, res){
+  // res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/user.html');
+})
+
+  .get('/master', function(req, res){
   // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/master.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  /*messages between clients are controlled here*/
   socket.on('disconnect', function(){
     console.log('user disconnected');
   })
@@ -25,6 +33,7 @@ io.on('connection', function(socket){
   });
 });
 
+/*start webserver on port 3000*/
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
