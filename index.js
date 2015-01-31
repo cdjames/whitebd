37f6bbd93779506215713414edf9cb6f40eeb4c7
Collin James <mail@collinjam.es>
2015-01-30 19:39:32 -0500

    changed something

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
+  console.log(req);
 })
 
   .get('/user', function(req, res){
