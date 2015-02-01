be5babd7ebd8afb4d6a81fa25451d44e6cf29ece
Collin James <james1cd@gmail.com>
2015-02-01 00:26:19 -0500

    figured out how to parse form data

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser')
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+// app.use(express.urlencoded()); // to support URL-encoded bodies
+var fs = require('fs');
+var obj;
 
 /*app.get forwards urls to specified html page*/
 app.get('/', function(req, res){
   // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
-  console.log(req);
+  // console.log(req);
 })
 
   .get('/user', function(req, res){
-  // res.send('<h1>Hello world</h1>');
-  res.sendFile(__dirname + '/user.html');
+    // res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/user.html');
 })
 
   .get('/master', function(req, res){
-  // res.send('<h1>Hello world</h1>');
-  res.sendFile(__dirname + '/master.html');
+    // res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/master.html');
+});
+app.post('/post', function(req, res){
+    res.send('<h1>Hello world</h1>' + req.body);
+    console.log("the request is...", req.body);
 });
 
 io.on('connection', function(socket){
