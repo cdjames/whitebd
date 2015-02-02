c7d9d67639348730e37c6b6b4378bea1c0dc3d13
Collin James <james1cd@gmail.com>
2015-02-02 16:32:11 -0500

    using cookies to implement login system

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 // app.use(express.urlencoded()); // to support URL-encoded bodies
-var fs = require('fs');
-var user_file;
+// var domain = require('domain');
+// var d = domain.create();
+var fs = require('fs'); // needed to read files
 
 /*app.get forwards urls to specified html page*/
 app.get('/', function(req, res){
@@ -26,23 +27,62 @@ app.get('/', function(req, res){
   .get('/master', function(req, res){
     // res.send('<h1>Hello world</h1>');
     res.sendFile(__dirname + '/master.html');
-});
+})
+  .get('/logout', function(req, res){
+    // res.send('<h1>Hello world</h1>');
+    res.cookie('login', 'null');
+    res.redirect('/');
+})
+  .get('/jquery.cookie.js', function(req, res){
+    res.sendFile(__dirname + '/jquery.cookie.js');
+  });
+/* handle login here */
 app.post('/post', function(req, res){
-    // res.send('<h1>Hello world</h1>' + req.body.username);
-    // console.log("the request is...", req.body);
     var filename = req.body.username + '.json';
+    var user_file;
+
+  // d.on('error', function(err) { // handles errors from readFile
+  //   if (err.errno == 34){       // i.e. "no file found"
+  //     console.error("file "+err.path+" does not exist");     
+  //     res.send('no_user');          
+  //   }
+  // });
+
+  // d.run(function() {
+    // if (fs.exists)
     fs.readFile(filename, 'utf8', function (err, data) {
-      if (err) throw err;
-      user_file = JSON.parse(data);
+      // console.log(err);
+      if (err) { // can't open file
+        // res.send('no_user');
+        res.cookie('login', 'no_user');
+        res.redirect('/');
+        return console.error(err);
+      }
+
+      user_file = JSON.parse(data); // get contents of file
+
+      if (user_file) { // user exists
+        if (user_file.password == req.body.password) { // good password
+          // res.sendFile(__dirname + '/user.html');
+          res.cookie('login', 'success');
+          res.redirect('/user');
+          // res.send({code: 'success', 
+          //           html: 'html'}
+          //           );
+          console.log("user " + req.body.username + " logged in.");
+        } else { // wrong password
+          // res.send('failure');
+          res.cookie('login', 'bad_password');
+          res.redirect('/');
+        }
+      } else {
+        res.cookie('login', 'no_user');
+        console.log("no such user.");
+        // res.send('no_user');
+        res.redirect('/');
+      }
     });
-    // if (user_file) {
-    if (user_file && user_file.password == req.body.password) {
-      res.sendFile(__dirname + '/user.html');
-      console.log("user " + req.body.username + " logged in.");
-    } else {
-      res.send('<script>alert("login failed");</script>')
-    }
-    // }
+  // });
 });
 
 io.on('connection', function(socket){
