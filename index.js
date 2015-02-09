ca3099a19ab6f5add9e585c98c47c3474358f50e
Collin James <james1cd@gmail.com>
2015-02-09 16:25:28 -0500

    login is working for teacher and user

--- index.js
+++ index.js
@@ -28,11 +28,29 @@ app.get('/', function(req, res){
     
     if (parsedUrl.query.name == req.cookies.username // there is a param name and it matches the cookie
         && req.cookies.login == 'success'){ // and the login was successful
-      res.sendFile(__dirname + '/user.html'); // send the user to the page
+      // if (req.cookies.teachers != "true") {
+        res.sendFile(__dirname + '/user.html'); // send the user to the page
+      // } else {
+      //   res.sendFile(__dirname + '/teacher.html'); // send the user to the page
+      // }
+      
     } else { // otherwise
       res.cookie('login', 'null'); // set the login cookie
       res.redirect('/'); // make them log in
-    }   
+    }
+})
+  .get('/teacher', function(req, res){ // request for example.com/user
+    var parsedUrl = url.parse(req.url, true); // parse url param (example.com/user?param=value)
+    // console.log("Cookies: ", req.cookies); // testing
+    // console.log(parsedUrl.query); // testing
+    
+    if (parsedUrl.query.name == req.cookies.username // there is a param name and it matches the cookie
+        && req.cookies.login == 'success'){ // and the login was successful
+      res.sendFile(__dirname + '/teacher.html'); // send the user to the page
+    } else { // otherwise
+      res.cookie('login', 'null'); // set the login cookie
+      res.redirect('/'); // make them log in
+    }  
 })
   .get('/master', function(req, res){
     // res.send('<h1>Hello world</h1>');
@@ -40,7 +58,8 @@ app.get('/', function(req, res){
 })
   .get('/logout', function(req, res){
     // res.send('<h1>Hello world</h1>');
-    res.cookie('login', 'null');
+    res.cookie('login', 'null')
+        .cookie('teachers', 'null');
     res.redirect('/');
 })
   .get('/jquery.cookie.js', function(req, res){
@@ -63,14 +82,20 @@ app.post('/post', function(req, res){
     // if successful
     user_file = JSON.parse(data); // get contents of file
     console.log("teachers are ",user_file.teachers.toString());
+    console.log("user_file.teachers is ",typeof user_file.teachers);
     if (user_file) { // user exists
       if (user_file.password == req.body.password) { // good password
         // cookie expires in 1 day
         res.cookie('login', 'success', { maxAge: 86400000 })
             .cookie('username', name, { maxAge: 86400000 })
-            .cookie('teachers',user_file.teachers.toString(),{ maxAge: 86400000 });
-        res.redirect('/user?name='+name); // need to specify name in url param to reach page
+            .cookie('teachers', user_file.teachers.toString(),{ maxAge: 86400000 });
         console.log("user " + name + " logged in.");
+        if (user_file.teachers != true){ // if not a teacher
+          res.redirect('/user?name='+name); // need to specify name in url param to reach page
+        } else { // if a teacher
+          res.redirect('/teacher?name='+name); // need to specify name in url param to reach page
+        }
+        
       } else { // wrong password
         res.cookie('login', 'bad_password', { maxAge: 300000 }); // cookie expires in 5 min
         res.redirect('/');
