eb7987231527187f1b5579538b8e1f11aa029dfc
Collin James <james1cd@gmail.com>
2015-09-15 22:10:22 -0400

    login is much more solid now. need to add ability for teacher to delete a post -- maybe if the post is blank it will be deleted?

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
   .get('/student', function(req, res){ // request for example.com/user
+    checkLogin(req, res);
     // var parsedUrl = url.parse(req.url, true); // parse url param (example.com/user?param=value)
     // // console.log("Cookies: ", req.cookies); // testing
     // // console.log(parsedUrl.query); // testing
@@ -30,7 +31,7 @@ app.get('/', function(req, res){
     // if (parsedUrl.query.name == req.cookies.username // there is a param name and it matches the cookie
     //     && req.cookies.login == 'success'){ // and the login was successful
       // if (req.cookies.teachers != "true") {
-        res.sendFile(__dirname + '/student.html'); // send the user to the page
+        // res.sendFile(__dirname + '/student.html'); // send the user to the page
       // } else {
       //   res.sendFile(__dirname + '/teacher.html'); // send the user to the page
       // }
@@ -40,6 +41,12 @@ app.get('/', function(req, res){
     //   res.redirect('/'); // make them log in
     })
   .get('/teacher', function(req, res){ // request for example.com/user
+      // if(checkLogin(req, res)) {
+      //   res.sendFile(__dirname + '/teacher.html'); // send the user to the page
+      // } else {
+
+      // }
+      checkLogin(req, res);
     // var parsedUrl = url.parse(req.url, true); // parse url param (example.com/user?param=value)
     // // console.log("Cookies: ", req.cookies); // testing
     // // console.log(parsedUrl.query); // testing
@@ -47,7 +54,7 @@ app.get('/', function(req, res){
     // if (parsedUrl.query.name == req.cookies.username // there is a param name and it matches the cookie
     //     && req.cookies.login == 'success'){ // and the login was successful
       // if (req.cookies.teachers != "true") {
-        res.sendFile(__dirname + '/teacher.html'); // send the user to the page
+        
       // } else {
       //   res.sendFile(__dirname + '/teacher.html'); // send the user to the page
       // }
@@ -78,22 +85,25 @@ app.post('/post', function(req, res){
       console.log("teacher is ",user_file.username.toString());
       // console.log("user_file.teachers is ",typeof user_file.teachers);
       if (user_file) { // user exists
-        if (user_file.password == req.body.password) { // good password
+        if (user_file.password == req.body.password && teacher != student) { // good password & not same name as teacher
           // cookie expires in 1 day
           res.cookie('login', 'success', { maxAge: 86400000 })
               .cookie('teacher', teacher, { maxAge: 86400000 })
               .cookie('student', student, { maxAge: 86400000 });
           console.log("user " + student + " logged in.");
           if (student != user_file.teacherPass){ // if not a teacher
+            res.clearCookie('isTeacher');
             res.redirect('/student?name='+student); // need to specify name in url param to reach page
           } else { // if a teacher
+            res.cookie('isTeacher', 'true', { maxAge: 86400000 });
             // console.log("games are ",user_file.game_names.toString());
             // res.cookie('games', user_file.game_names.toString(),{ maxAge: 86400000 });
             res.redirect('/teacher?name='+teacher); // need to specify name in url param to reach page
           }
           
-        } else { // wrong password
-          bad_login(res, 'bad_password');
+        } else { // wrong password or bad name
+          if(teacher == student){bad_login(res, 'bad_name');}
+          else {bad_login(res, 'bad_password');}
         }
       } else {
         bad_login(res, 'no_user');
@@ -126,4 +136,26 @@ function bad_login (res, msg) {
   res.cookie('login', msg, { maxAge: 300000 });
   console.log(msg);
   res.redirect('/');
+}
+
+function checkLogin(req, res) {
+    var parsedUrl = url.parse(req.url, true); // parse url param (example.com/user?param=value)
+    // console.log("Line 141 Cookies: ", req.cookies); // testing
+    // console.log(parsedUrl.query); // testing
+    
+    if ((parsedUrl.query.name == req.cookies.student // there is a param name and it matches the cookie
+        || parsedUrl.query.name == req.cookies.teacher)
+        && req.cookies.login == 'success'){ // and the login was successful
+      // console.log("line 148 passed first if");
+      if (req.cookies.isTeacher == "true") {
+        // console.log("line 150 passed 2nd if");
+        res.sendFile(__dirname + '/teacher.html'); // send the user to the page
+        return;
+      } else {
+        res.sendFile(__dirname + '/student.html'); // send the user to the page
+      }   
+    } else { // otherwise
+      res.cookie('login', 'null'); // set the login cookie
+      res.redirect('/'); // make them log in
+    }
 }
