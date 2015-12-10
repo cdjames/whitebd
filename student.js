dfe84e7554c61d95051bda575cba8ebe6c178456
Collin James <james1cd@gmail.com>
2015-12-10 18:01:08 -0500

    trying to convert to js files

---
+++ student.js
@@ -0,0 +1,37 @@
+var socket = io(),
+          message,
+          studentName = $.cookie('student');
+
+console.log(studentName);
+
+$('form').submit(function(e){
+  e.preventDefault();
+  message = $('#m').val();
+
+  if(message.toLowerCase() == "clear()") { 
+    $('#messages').html(''); 
+    $('#m').val('');
+  }
+  else {
+    socket.emit('student text', {'message': message, 'sender': studentName});
+    $('#m').val('');
+    // return false;
+  }
+});
+
+socket.on('student text', function(data){
+  if (data.sender == studentName) 
+  {
+      var $message = $("<li>"+ data.message 
+                        + "<span><i> -- " + data.sender + "</i></span>" 
+                      + "</li>");
+      // $('#messages').append($('<li>').text(data.message));
+      $('#messages').append($message);
+      scrollBottom(); // scrolls to bottom of page
+  }
+  
+});
+
+function scrollBottom() {
+  window.scrollTo(0,document.body.scrollHeight);
+}
